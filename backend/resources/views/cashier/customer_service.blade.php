86207523a7ebb2281e387f6035251bfde3bc410c
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2026-01-17 13:37:47 +0700

    hapus backend dari branch frontend

--- backend/resources/views/cashier/customer_service.blade.php
+++
@@ -1,3 +0,0 @@
-{{-- This file appears to be unused and contains incorrect references to admin routes --}}
-{{-- The actual customer service view for cashier is located at cashier/customer_service/index.blade.php --}}
-{{-- This file has been deprecated and replaced with the correct implementation --}}
