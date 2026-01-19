1e1c15db5d6d7304f64177dba05fcbefd513cce3
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2026-01-19 21:00:58 +0700

    fix error add to cart

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,8 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
         
         $middleware->validateCsrfTokens(except: [
-            '/api/login',
-            '/api/register',
+            '/api/*',
         ]);
 
         $middleware->alias([
